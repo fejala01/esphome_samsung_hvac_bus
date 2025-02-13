dc34d9a3e03862c79437142ba78fb8bfacf4ce52
Felix <felix.lamping@gmx.de>
2025-02-13 16:09:24 +0100

    update

--- components/samsung_ac/protocol_nasa.cpp
+++ components/samsung_ac/protocol_nasa.cpp
@@ -464,6 +464,9 @@ namespace esphome
             {
                 MessageSet targetoffset(MessageNumber::VAR_in_target_offset);
                 targetoffset.value = request.target_offset.value() * 10.0;
+                if (targetoffset.value < 0) {
+                    targetoffset.value = 65535 + targetoffset.value;
+                }
                 packet.messages.push_back(targetoffset);
             }
 
@@ -982,6 +985,10 @@ namespace esphome
             case MessageNumber::VAR_in_target_offset: // unit = 'Celsius' from XML
             {
                 double temp = (double)message.value;
+                if (temp > 60000) {
+                    temp = temp - 65535;
+                }
+                temp = temp / (double)10;
                 LOG_MESSAGE(VAR_in_target_offset, temp, source, dest);
                 target->set_target_offset(source, temp);
                 break;
