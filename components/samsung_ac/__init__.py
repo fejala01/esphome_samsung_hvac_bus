d88b87674a756a3dcc067f44478863dfd5f62d98
Felix <felix.lamping@gmx.de>
2025-02-13 01:17:37 +0100

    update

--- components/samsung_ac/__init__.py
+++ components/samsung_ac/__init__.py
@@ -265,7 +265,7 @@ DEVICE_SCHEMA = cv.Schema(
             device_class=DEVICE_CLASS_TEMPERATURE,
             state_class=STATE_CLASS_MEASUREMENT,
         ),
-        cv.Optional(CONF_DEVICE_OUTDOOR_TEMPERATURE): sensor.sensor_schema(
+        cv.Optional(CONF_DEVICE_OUTDOOR_WATER_TEMPERATURE): sensor.sensor_schema(
             unit_of_measurement=UNIT_CELSIUS,
             accuracy_decimals=1,
             device_class=DEVICE_CLASS_TEMPERATURE,
