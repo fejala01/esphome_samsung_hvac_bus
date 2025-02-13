9af6d16cf342d5c6f5ad66d53633e075cac60aa0
Felix <felix.lamping@gmx.de>
2025-02-13 15:35:15 +0100

    update

--- components/samsung_ac/protocol_nasa.cpp
+++ components/samsung_ac/protocol_nasa.cpp
@@ -465,7 +465,7 @@ namespace esphome
                 MessageSet targetoffset(MessageNumber::VAR_in_target_offset);
                 targetoffset.value = request.target_offset.value() * 10.0;
                 if (targetoffset.value < 0) {
-                    temp = 6554 + targetoffset.value;
+                    targetoffset = 6554 + targetoffset.value;
                 }
                 packet.messages.push_back(targetoffset);
             }
