2ef63b2e9dc7926205047693a85f3567b5acbf06
Felix <felix.lamping@gmx.de>
2025-03-04 14:31:53 +0100

    update

--- components/samsung_ac/protocol_nasa.cpp
+++ components/samsung_ac/protocol_nasa.cpp
@@ -9,7 +9,7 @@
 #include "esphome/core/application.h"
 #include "esphome/components/sensor/sensor.h"
 
-static int packet_fail_count = 0;  // Fehlerzähler für gescheiterte Pakete
+static int packet_fail = 0;  // Fehlerzähler für gescheiterte Pakete
 
 esphome::samsung_ac::Packet packet_;
 
@@ -2165,9 +2165,9 @@ namespace esphome
                 else if (it->retry_count >= 3) {
                     ESP_LOGW(TAG, "Packet %d failed after %d retries. Removing from list.", it->packet.command.packetNumber, it->retry_count);
                     it = sent_packets.erase(it);  // `erase()` gibt den nächsten gültigen Iterator zurück
-                    packet_fail_count++;
+                    packet_fail++;
                     if (id(packet_fail_count_sensor) != nullptr) {
-                        id(packet_fail_count_sensor).publish_state
+                        id(packet_fail_count_sensor).publish_state(packet_fail)
                     
 
                 } 
