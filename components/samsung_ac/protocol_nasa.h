c0163cc2f80c3577ce99355231bd027d294a1e84
Felix <felix.lamping@gmx.de>
2025-03-16 21:25:54 +0100

    update

--- components/samsung_ac/protocol_nasa.h
+++ components/samsung_ac/protocol_nasa.h
@@ -293,6 +293,7 @@ namespace esphome
             NasaProtocol() = default;
 
             void publish_request(MessageTarget *target, const std::string &address, ProtocolRequest &request) override;
+            void publish_fsv_read(MessageTarget *target, const std::string &address) override;
             void protocol_update(MessageTarget *target) override;
         };
 
