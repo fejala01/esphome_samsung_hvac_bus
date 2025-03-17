46d764cb7554a131d86b644bf75e0c3c45be6a66
Felix <felix.lamping@gmx.de>
2025-03-17 12:59:17 +0100

    update

--- components/samsung_ac/protocol_nasa.cpp
+++ components/samsung_ac/protocol_nasa.cpp
@@ -752,13 +752,13 @@ namespace esphome
                     std::this_thread::sleep_for(std::chrono::seconds(2)); // Verzögerung von 2 Sekunden
                     
                     // Schalter ausschalten
-                    update_fsv_read(0);
+                    update_fsv_read(false);
 
                     // Kurze Verzögerung, um den Schalter auszuschalten
                     std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Verzögerung von 500 Millisekunden
 
                     // Schalter wieder einschalten
-                    update_fsv_read(1);
+                    update_fsv_read(true);
                 });
             }
 
