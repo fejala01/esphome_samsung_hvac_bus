275292306305c09b3ba1bcb85a2d617177f27edd
Felix <felix.lamping@gmx.de>
2025-03-17 13:08:34 +0100

    update

--- components/samsung_ac/protocol.h
+++ components/samsung_ac/protocol.h
@@ -78,6 +78,7 @@ namespace esphome
             virtual void register_address(const std::string address) = 0;
             virtual void set_power(const std::string address, bool value) = 0;
             virtual void set_power_zone2(const std::string address, bool value) = 0;
+            virtual void set_fsv_read(bool value) = 0;
 
             virtual void set_operation(const std::string address, bool value) = 0;
             virtual void set_vacation(const std::string address, bool value) = 0;
--- components/samsung_ac/samsung_ac_device.h
+++ components/samsung_ac/samsung_ac_device.h
@@ -1812,6 +1812,7 @@ namespace esphome
 
       optional<bool> _cur_power;
       optional<bool> _cur_power_zone2;
+      optional<bool> _cur_fsv_read;
       optional<bool> _cur_operation;
       optional<bool> _cur_vacation;
       optional<bool> _cur_fsv3041;
@@ -1837,6 +1838,12 @@ namespace esphome
           power_zone2->publish_state(value);
         if (climate != nullptr)
           calc_and_publish_mode();
+      }
+      void update_fsv_read(bool value)
+      {
+        _cur_fsv_read = value;
+        if (fsv_read != nullptr)
+          fsv_read->publish_state(value);
       }
       void update_operation(bool value)
       {
