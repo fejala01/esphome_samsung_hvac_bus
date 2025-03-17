75893ffe0e57781becf94a774548672df1efce7c
Felix <felix.lamping@gmx.de>
2025-03-17 16:29:03 +0100

    update

--- components/samsung_ac/protocol_nasa.cpp
+++ components/samsung_ac/protocol_nasa.cpp
@@ -995,13 +995,10 @@ namespace esphome
             if (request.fsv2011)
             {
                 packet = Packet::createa_partial(Address::parse(address), DataType::Read);
-                  // Asynchrones Delay von 2 Sekunden
-                    std::async(std::launch::async, [this]() {std::this_thread::sleep_for(std::chrono::seconds(2));
                         
-                        MessageSet fsv_read0(MessageNumber::VAR_in_fsv2011);
-                        fsv_read0.value = 0;
-                        packet.messages.push_back(fsv_read0);
-                    });
+                MessageSet fsv_read0(MessageNumber::VAR_in_fsv2011);
+                fsv_read0.value = 0;
+                packet.messages.push_back(fsv_read0);
 
             }
 
