1aff565d23f3e35b92676ac1dc9fc040a1f98101
Felix <felix.lamping@gmx.de>
2025-03-17 09:57:31 +0100

    update

--- components/samsung_ac/protocol_nasa.cpp
+++ components/samsung_ac/protocol_nasa.cpp
@@ -2335,7 +2335,7 @@ namespace esphome
                         ESP_LOGW(TAG, "found Response for packet number %d", it->packet.command.packetNumber);
                         sent_packets.erase(it);
                         response_found = true;
-                        
+                        break;
                     }
                 }
             }
