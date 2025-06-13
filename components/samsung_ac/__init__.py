46f8b602f8d973e79be41ff67ba52abea4260243
Felix <felix.lamping@gmx.de>
2025-06-13 20:08:17 +0200

    update

--- components/samsung_ac/__init__.py
+++ components/samsung_ac/__init__.py
@@ -51,7 +51,6 @@ SELECT_WATER_HEATER_MODE_SCHEMA = select.select_schema(
 NUMBER_SCHEMA = number.NUMBER_SCHEMA.extend(
     {cv.GenerateID(): cv.declare_id(Samsung_AC_Number)}
 )
-)
 
 CLIMATE_SCHEMA = climate.CLIMATE_SCHEMA.extend(
     {cv.GenerateID(): cv.declare_id(Samsung_AC_Climate)}
